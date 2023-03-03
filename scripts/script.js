8e01f53ef38db2c257bccb8a6e5f595556bfdcb4
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-03-03 17:09:20 +0300

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// const promptResult = prompt("Do you want Iphone 14 pro? (yes/no)");
+
+// function question(answer) {
+//   if (answer === "yes") {
+//     alert("Thank you!");
+//   } else {
+//     alert("Sorry to hear that.");
+//   }
+// }
+
+// question(promptResult);
